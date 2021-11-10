4538b4d9d0d5e58416fdb11f4daef01bb74547f6
Honza Sušek <honza@susek.cz>
2021-11-10 11:46:49 +0100

    add eslint-config-typescript package

---
+++ packages/eslint-config-typescript/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: ["@typescript-eslint", "prettier", "unused-imports"],
+  extends: ["@strv/typescript", "@strv/typescript/style", "prettier"],
+  rules: {
+    "import/order": ["error", { "newlines-between": "never" }],
+    "prettier/prettier": "error",
+    "import/no-default-export": 0,
+    "consistent-return": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
