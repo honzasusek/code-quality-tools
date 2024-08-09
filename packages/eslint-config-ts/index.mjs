1fcffa22800bc784f88f3dff27de06c3512bedf3
Honza Sušek <honza.susek@strv.com>
2024-08-09 09:35:04 +0200

    eslint-config-ts: fix unused-imports

--- packages/eslint-config-ts/index.mjs
+++ packages/eslint-config-ts/index.mjs
@@ -1,17 +1,17 @@
-import _import from 'eslint-plugin-import'
-import prettier from 'eslint-plugin-prettier'
-import { fixupPluginRules } from '@eslint/compat'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
-import unusedImports from 'eslint-plugin-unused-imports'
+import _import from "eslint-plugin-import"
+import prettier from "eslint-plugin-prettier"
+import { fixupPluginRules } from "@eslint/compat"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+import js from "@eslint/js"
+import { FlatCompat } from "@eslint/eslintrc"
+import unusedImports from "eslint-plugin-unused-imports"
 
-import lineSpacing from '@honzasusek/eslint-config-line-spacing'
-import strvTypescript from '@strv/eslint-config-typescript'
-import strvTypescriptOptional from '@strv/eslint-config-typescript/optional'
+import lineSpacing from "@honzasusek/eslint-config-line-spacing"
+import strvTypescript from "@strv/eslint-config-typescript"
+import strvTypescriptOptional from "@strv/eslint-config-typescript/optional"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -35,12 +35,13 @@ export default [
     removeImportFromPlugins(strvTypescript),
     removeImportFromPlugins(strvTypescriptOptional),
     {
-      plugins: { prettier, unusedImports},      
+      plugins: { prettier, "unused-imports": unusedImports },      
       rules: {
         "import/order": ["error", { "newlines-between": "never" }],
         "prettier/prettier": "error",
         "import/no-default-export": 0,
         "consistent-return": "off",
+        "@typescript-eslint/no-unused-vars": "off",
         "unused-imports/no-unused-imports": "error",
         "unused-imports/no-unused-vars": [
           "warn",
