ccc8f648822e7f4888b5b1ad4a7c3bd7cf5a6694
Honza Sušek <honza.susek@strv.com>
2024-09-20 16:04:13 +0200

    add stylelint config

---
+++ packages/stylelint-config-styled-components/index.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: [
+    'stylelint-config-recommended',
+    'stylelint-config-recommended-scss',
+  ],
+  rules: {
+    'no-descending-specificity': null,
+    'font-family-no-missing-generic-family-keyword': null,
+    'selector-pseudo-element-no-unknown': null,
+    'no-empty-source': null,
+    'scss/operator-no-newline-after': null,
+    'scss/operator-no-unspaced': null,
+  },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      customSyntax: 'postcss-styled-syntax',
+    },
+  ],
+}
